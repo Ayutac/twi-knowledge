ed393c8ea7c41df4ac5c96eb575db8a468b7e0c3
ayutac <fly.high.android@gmail.com>
2024-01-28 19:37:03 +0100

    Improved vol comparator.

--- src/main/java/org/abos/twi/knowledge/core/Volume.java
+++ src/main/java/org/abos/twi/knowledge/core/Volume.java
@@ -11,6 +11,10 @@ public record Volume(String name, String wikiLink) implements Comparable<Volume>
 
     @Override
     public int compareTo(final Volume other) {
+        final int cmpLength = name.length() - other.name.length();
+        if (cmpLength != 0) {
+            return cmpLength;
+        }
         return name.compareTo(other.name);
     }
 }
