a5e7ae4d919d3cbd7ffbd0869c99ed6c1fc8d72c
ayutac <fly.high.android@gmail.com>
2024-01-30 05:48:18 +0100

    Fixed Book order

--- src/main/java/org/abos/twi/knowledge/core/Book.java
+++ src/main/java/org/abos/twi/knowledge/core/Book.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 
 public record Book(String name, Integer volumeOrd, Volume volume, String wikiLink, String publicationLink, LocalDate publicationDate, String audibleLink, LocalDate audibleDate) implements Named, Comparable<Book> {
 
+    private static final String BOOK1 = "Book 1";
+
     public Book(final String name, final Integer volumeOrd, final Volume volume, final String wikiLink, final String publicationLink, final LocalDate publicationDate, final String audibleLink, final LocalDate audibleDate) {
         this.name = Objects.requireNonNull(name);
         if (volume == null ^ volumeOrd == null) {
@@ -23,6 +25,17 @@ public record Book(String name, Integer volumeOrd, Volume volume, String wikiLin
 
     @Override
     public int compareTo(final Book other) {
+        // book 1 needs special consideration because Vol 1 (Archived) is last volume
+        if (this.name.startsWith(BOOK1)) {
+            if (other.name.startsWith(BOOK1)) {
+                return name.compareTo(other.name);
+            }
+            return -1;
+        }
+        if (other.name.startsWith((BOOK1))) {
+            return 1;
+        }
+        // sort by volume first
         if (volume != null) {
             if (other.volume == null) {
                 return -1;
@@ -37,6 +50,7 @@ public record Book(String name, Integer volumeOrd, Volume volume, String wikiLin
                 return 1;
             }
         }
+        // sort by name second
         return name.compareTo(other.name);
     }
 
